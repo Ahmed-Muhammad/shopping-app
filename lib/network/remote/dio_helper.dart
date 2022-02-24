5b55df117fe28e3621922830f3079d8f2bfd732f
Ahmad M. Hassanien <44645999+Ahmed-Muhammad@users.noreply.github.com>
2022-02-24 13:48:31 +0200

    Dio

--- lib/network/remote/dio_helper.dart
+++ lib/network/remote/dio_helper.dart
@@ -15,16 +15,17 @@ class DioHelper {
   }
 
   //----------------------------Post Data----------------------------------------
-
   static Future<Response> postData({
     required String url,
     Map<String, dynamic>? query,
     required Map<String, dynamic> data,
     String lang = 'ar',
+    String? token,
   }) async {
-    dio!.options = BaseOptions(
-      headers: {'lang': lang},
-    );
+    dio!.options.headers = {
+      'lang': lang,
+      'Authorization': token,
+    };
     return await dio!.post(
       url,
       queryParameters: query,
@@ -33,11 +34,17 @@ class DioHelper {
   }
 
 //----------------------------Get Data----------------------------------------
-
   static Future<Response> getData({
     required String url,
     required Map<String, dynamic> query,
+    String lang = 'ar',
+    String? token,
   }) async {
+    //لما اعوز احط headers مختلفه عن اللي موجوده في في تعريف dio
+    dio!.options.headers = {
+      'lang': lang,
+      'Authorization': token,
+    };
     return await dio!.get(
       url,
       queryParameters: query,
